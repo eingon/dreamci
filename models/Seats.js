c06556e9f4ac4ed6dcabb02e93cd43e0295715ea
Ei Ngon Pwint Phoo <eingon21ptn@gmail.com>
2024-03-05 08:29:55 +0630

    add project

---
+++ models/Seats.js
@@ -0,0 +1,49 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+const SeatSchema=new Schema({
+    cinemaId:{
+        type:Schema.Types.ObjectId,
+        ref: "Cinemas",
+    },
+    row:{
+        type:String,
+        required:true,
+    },
+    no:{
+        type:String,
+        required:true,
+    },
+    status:{
+        type:Number,
+        default:1, 
+    },
+    screenNo:{
+        type: Number,
+        required: true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    noOfSeat:{
+        type: Number,
+        default: 1,
+    },
+    createdBy:{
+        type:Schema.Types.ObjectId,
+        ref:"Admins",
+    },
+    updatedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"Admins",
+    },
+    created:{
+        type:Date,
+        default:Date.now(),
+    },
+    updated:{
+        type:Date,
+        default:Date.now(),
+    },
+});
+module.exports=mongoose.model("Seats",SeatSchema);
